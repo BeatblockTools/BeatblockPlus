dc6b475ac7aa86c551305a21c6fe0206b7d3e198
ConfiG <cgytrus@cgyt.ru>
2025-10-02 11:30:12 +0300

    inverse modpath check

--- mod-loader.lua
+++ mod-loader.lua
@@ -93,94 +93,96 @@ function loadMods() -- loads mod data, assets, mod icons etc.
 
 	for _, modId in ipairs(love.filesystem.getDirectoryItems(modsPath)) do
 		local modPath = modsPath .. "/" .. modId
-		if love.filesystem.getInfo(modPath, 'directory') then
-			-- load mod data if it exists
-			local modData
-			if love.filesystem.getInfo(modPath .. "/mod.json", 'file') then
-				modData = readJsonFromFile(modPath .. "/mod.json")
-				registerMod(modData)
+		if not love.filesystem.getInfo(modPath, 'directory') then
+			goto continue
+		end
+
+		-- load mod data if it exists
+		local modData
+		if love.filesystem.getInfo(modPath .. "/mod.json", 'file') then
+			modData = readJsonFromFile(modPath .. "/mod.json")
+			registerMod(modData)
+		end
+
+		-- load mod icon if it exists
+		if love.filesystem.getInfo(modPath .. "/icon.png", 'file') then
+			local modIcon = love.graphics.newImage(modPath .. "/icon.png")
+			local width, height = modIcon:getDimensions()
+			if width ~= 73 or height ~= 33 then
+				print("[BB+] Mod " .. modId .. " has invalid icon size. Mod icons must be 73x33.")
+			else
+				modIcons[modId] = modIcon
 			end
+		end
 
-			-- load mod icon if it exists
-			if love.filesystem.getInfo(modPath .. "/icon.png", 'file') then
-				local modIcon = love.graphics.newImage(modPath .. "/icon.png")
-				local width, height = modIcon:getDimensions()
-				if width ~= 73 or height ~= 33 then
-					print("[BB+] Mod " .. modId .. " has invalid icon size. Mod icons must be 73x33.")
-				else
-					modIcons[modId] = modIcon
-				end
-			end
+		if modData ~= nil and not modData.enabled then
+			goto continue
+		end
 
-			if modData ~= nil and not modData.enabled then
-				goto continue
-			end
-
-			-- load assets
-			local assetsPath = modPath .. "/assets"
-			if love.filesystem.getInfo(assetsPath, 'directory') then
-				-- load sprites
-				loopFiles(sprites, assetsPath .. "/textures", function(tbl, path, fileName)
-					print("[BB+] injecting sprite " .. path .. "...")
-					tbl[fileName] = love.graphics.newImage(path)
-				end)
-
-				-- load sounds
-				loopFiles(sounds, assetsPath .. "/sounds", function(tbl, path, fileName)
-					print("[BB+] injecting sound " .. path .. "...")
-					tbl[fileName] = love.sound.newSoundData(path)
-				end)
-
-				-- load shaders
-				loopFiles(shaders, assetsPath .. "/shaders", function(tbl, path, fileName)
-					print("[BB+] injecting shader " .. path .. "...")
-					tbl[fileName] = love.graphics.newShader(path)
-				end)
-
-				-- load animations
-				loopFiles(animations, assetsPath .. "/animations", function(tbl, path, fileName)
-					if path:endswith(".png") then
-						print("[BB+] injecting animation " .. path .. "...")
-						local data = getParent(path) .. "data.json"
-						if not love.filesystem.getInfo(data, 'file') then
-							print("[BB+] Error while injecting animation '" .. path .. "'. The '" .. data .. "' file is missing!")
-						end
-						tbl[fileName] = ez.newjson(path, data)
-					end
-				end)
-
-				loopFiles(loc.json, assetsPath .. "/lang", function(tbl, path, fileName)
-					table.insert(customLanguages, fileName)
-					-- make sure we don't load english lang when owo is selected
-					if fileName == savedata.options.language then
-						print("[BB+] injecting lang file " .. path .. "...")
-						local modLoc = dpf.loadJson(path, {})
-						mergeLangFiles(loc.json, modLoc)
-					end
-				end)
-			end
-
-			-- load states
-			loopFiles({}, modPath .. "/states", function(_, path, fileName)
-				print("[BB+] injecting state " .. path .. "...")
-				bs.fromPath(fileName, path)
+		-- load assets
+		local assetsPath = modPath .. "/assets"
+		if love.filesystem.getInfo(assetsPath, 'directory') then
+			-- load sprites
+			loopFiles(sprites, assetsPath .. "/textures", function(tbl, path, fileName)
+				print("[BB+] injecting sprite " .. path .. "...")
+				tbl[fileName] = love.graphics.newImage(path)
 			end)
 
-			-- load entities
-			loopFiles({}, modPath .. "/entities", function(_, path, fileName)
-				print("[BB+] injecting entity " .. path .. "...")
-				em.new(path, fileName)
+			-- load sounds
+			loopFiles(sounds, assetsPath .. "/sounds", function(tbl, path, fileName)
+				print("[BB+] injecting sound " .. path .. "...")
+				tbl[fileName] = love.sound.newSoundData(path)
 			end)
 
-			-- load and call main.lua
-			if love.filesystem.getInfo(modPath .. "/main.lua") then
-				local chunk, errormsg = love.filesystem.load(modPath .. "/main.lua")
-				if errormsg then
-					print("[BB+] Error while loading the main.lua file of '" .. modId .. "': " .. errormsg)
-				else
-					local env = setmetatable({ modId = modId, modPath = modPath, modData = modData }, { __index = _G })
-					setfenv(chunk, env)()
+			-- load shaders
+			loopFiles(shaders, assetsPath .. "/shaders", function(tbl, path, fileName)
+				print("[BB+] injecting shader " .. path .. "...")
+				tbl[fileName] = love.graphics.newShader(path)
+			end)
+
+			-- load animations
+			loopFiles(animations, assetsPath .. "/animations", function(tbl, path, fileName)
+				if path:endswith(".png") then
+					print("[BB+] injecting animation " .. path .. "...")
+					local data = getParent(path) .. "data.json"
+					if not love.filesystem.getInfo(data, 'file') then
+						print("[BB+] Error while injecting animation '" .. path .. "'. The '" .. data .. "' file is missing!")
+					end
+					tbl[fileName] = ez.newjson(path, data)
 				end
+			end)
+
+			loopFiles(loc.json, assetsPath .. "/lang", function(tbl, path, fileName)
+				table.insert(customLanguages, fileName)
+				-- make sure we don't load english lang when owo is selected
+				if fileName == savedata.options.language then
+					print("[BB+] injecting lang file " .. path .. "...")
+					local modLoc = dpf.loadJson(path, {})
+					mergeLangFiles(loc.json, modLoc)
+				end
+			end)
+		end
+
+		-- load states
+		loopFiles({}, modPath .. "/states", function(_, path, fileName)
+			print("[BB+] injecting state " .. path .. "...")
+			bs.fromPath(fileName, path)
+		end)
+
+		-- load entities
+		loopFiles({}, modPath .. "/entities", function(_, path, fileName)
+			print("[BB+] injecting entity " .. path .. "...")
+			em.new(path, fileName)
+		end)
+
+		-- load and call main.lua
+		if love.filesystem.getInfo(modPath .. "/main.lua") then
+			local chunk, errormsg = love.filesystem.load(modPath .. "/main.lua")
+			if errormsg then
+				print("[BB+] Error while loading the main.lua file of '" .. modId .. "': " .. errormsg)
+			else
+				local env = setmetatable({ modId = modId, modPath = modPath, modData = modData }, { __index = _G })
+				setfenv(chunk, env)()
 			end
 		end
 		::continue::
