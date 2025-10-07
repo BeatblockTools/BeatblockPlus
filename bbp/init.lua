4f3858706148c01ac5ca261b5e705ba7a02acb93
ConfiG <cgytrus@cgyt.ru>
2025-10-07 19:10:15 +0300

    lua 5.2 compat

--- bbp/init.lua
+++ bbp/init.lua
@@ -3,6 +3,24 @@ local bbp = {}
 bbp.utils = require("bbp.utils")
 bbp.loader = require("bbp.loader")
 
+local isLua51 = rawlen == nil
+if not isLua51 then
+    bbp.mods = setmetatable({}, {
+        __index = function(_, k)
+            return bbp.loader.mods[k]
+        end,
+        __len = function(_)
+            return #bbp.loader.mods
+        end,
+        __pairs = function(_)
+            return pairs(bbp.loader.mods)
+        end,
+        __ipairs = function(_)
+            return ipairs(bbp.loader.mods)
+        end
+    })
+end
+
 bbp.config = setmetatable({}, {
     __index = function(_, k)
         return bbp.loader.mods[k].config
@@ -13,10 +31,10 @@ bbp.config = setmetatable({}, {
     end
 })
 
--- do this because the __len metamethod doesn't work in lua 5.1
 return setmetatable({}, {
     __index = function(_, k)
-        if k == "mods" then
+        -- do this because the __len metamethod doesn't work in lua 5.1
+        if isLua51 and k == "mods" then
             return bbp.loader.mods
         end
         return bbp[k]
