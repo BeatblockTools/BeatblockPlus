cca54eb3d4c0497bfe119aa6a9f8b8d4b1f5e2a9
ConfiG <cgytrus@cgyt.ru>
2025-10-07 18:31:20 +0300

    fix bbp.mods once again

--- bbp/init.lua
+++ bbp/init.lua
@@ -3,16 +3,25 @@ local bbp = {}
 bbp.utils = require("bbp.utils")
 bbp.loader = require("bbp.loader")
 
-bbp.mods = setmetatable({}, {
-    __index = function(_, k)
-        return bbp.loader.mods[k]
-    end
-})
-
 bbp.config = setmetatable({}, {
     __index = function(_, k)
         return bbp.loader.mods[k].config
+    end,
+    -- this doesn't work, thanks lua 😊
+    __len = function(_)
+        return #bbp.loader.mods
     end
 })
 
-return bbp
+-- do this because the __len metamethod doesn't work in lua 5.1
+return setmetatable({}, {
+    __index = function(_, k)
+        if k == "mods" then
+            return bbp.loader.mods
+        end
+        return bbp[k]
+    end,
+    __newindex = function (_, _, _)
+        error("Cannot set members of 'bbp'")
+    end
+})
